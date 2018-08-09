f2e5c7310761c7fda5177d2dc87b381ef7535939
Hannes Dahlberg <hannes@dwik.se>
2018-08-09 14:12:14 +0200

    Removed unused files and added binary executable

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,15 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var model = require("./modules/orm/model");
-var relation = require("./modules/orm/relation");
-var statement = require("./modules/orm/statement");
-var helpers = require("./modules/helpers");
-var prom = require("./modules/prom");
-var storage = require("./modules/storage");
-exports.Artoo = {
-    ORM: __assign({}, model, relation, statement),
-    helpers: helpers,
-    Prom: prom.Prom,
-    prom: prom,
-    storage: storage
-};
+var _model = require("./modules/orm/model");
+var _helpers = require("./modules/helpers");
+var _prom = require("./modules/prom");
+var _storage = require("./modules/storage");
+var ORM;
+(function (ORM) {
+    ORM.model = _model;
+    ORM.Model = _model.Model;
+})(ORM = exports.ORM || (exports.ORM = {}));
+exports.helpers = _helpers;
+exports.Prom = _prom.Prom;
+exports.prom = _prom;
+exports.storage = _storage;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-import * as model from './modules/orm/model';
-import * as relation from './modules/orm/relation';
-import * as statement from './modules/orm/statement';
+import * as _model from './modules/orm/model';
+import * as _relation from './modules/orm/relation';
+import * as _statement from './modules/orm/statement';
 
-import * as helpers from './modules/helpers'
-import * as migrate from './modules/migrate';
-import * as prom from './modules/prom';
-import * as storage from './modules/storage';
+import * as _helpers from './modules/helpers';
+import * as _prom from './modules/prom';
+import * as _storage from './modules/storage';
 
-export let Artoo = {
-    ORM: {
-        ...model,
-        ...relation,
-        ...statement
-    },
-    helpers,
-    Prom: prom.Prom,
-    prom: prom,
-    storage
+export namespace ORM {
+    export const model = _model;
+    export const Model = _model.Model;
 }
+
+export const helpers = _helpers;
+export const Prom = _prom.Prom;
+export const prom = _prom;
+export const storage = _storage;
