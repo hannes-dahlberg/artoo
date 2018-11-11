db947e9f7124d929271e7b29cbd17644830cea57
Hannes Dahlberg <hannes@dwik.se>
2018-11-11 23:57:02 +0100

    WOP Factory

--- src/modules/factory.ts
+++ src/modules/factory.ts
@@ -1,13 +1,68 @@
 import { Singleton } from './singleton';
 import * as helpers from './helpers';
 
-type reference<T extends { name: string }> = {
-  instance: instanceType<T>,
+type reference<T> = {
+  name: string,
+  classInstance: new(...args: any[]) => T,
+  objectInstance: T | null
+}
+
+class Container extends Singleton {
+  private references: reference<any>[] = [];
+
+  public create<T>(name: string, ...args: any[]): T {
+    let reference: reference<T> = this.references.find((reference: reference<T>) => reference.name == name);
+    return new (reference.classInstance)(args);
+  }
+  public set<T>(classInstance: new(...args: any[]) => any): void {
+    let referenceIndex: number = this.references.findIndex((reference: reference<T>) => reference.name == name);
+    let reference: reference<T> = { name: classInstance.name, classInstance, objectInstance: null};
+    if(referenceIndex == -1) {
+      this.references.push(reference);
+    } else {
+      this.references[referenceIndex] = reference
+    }
+  }
+  public get<T>(name: string): T {
+    let reference: reference<T> = this.references.find((reference: reference<T>) => reference.name == name);
+    if(reference != undefined) {
+      if(reference.objectInstance === null) {
+        reference.objectInstance = new(reference.classInstance)();
+      }
+      return reference.objectInstance;
+    }
+
+    throw new Error('Reference could not be found');
+  }
+
+  public getClassInstance<T>(name: string): new(...args: []) => T {
+    let reference = this.references.find((reference: reference<T>) => reference.name == name);
+    if(reference != undefined) { return reference.classInstance; }
+
+    throw new Error('Reference could not be found');
+  }
+}
+
+export let container = Container.getInstance<Container>();
+
+export const injector = <T>() => (instance: new() => T) => {
+  container.set<T>(instance);
+};
+
+export const inject = <T>(name: string) => (instance: Function) => {
+  
+};
+
+export const injectNew = <T>(name: string) => (instance: Function)
+
+/*type reference<T> = {
+  instance: new() => T,
+  object?: T,
   name: string,
   args?: any[]
 }
 
-type instanceType<T extends { name: string}> = T | object | string;
+type instanceType<T> = (new() => T) | object | string;
 type refererenceOptions = {
   name?: string,
   args?: any[]
@@ -17,8 +72,8 @@ export class Container extends Singleton {
   private references: reference<any>[] = [];
 
   //Create a new object of a reference or add reference if not existing
-  public create<T extends { name: string }>(instance: string, options: refererenceOptions) : T {
-    let reference: reference<T> | false = this.getReference(this.getName(instance, options));
+  public create<T>(instance: instanceType<T>, options: refererenceOptions) : T {
+    let reference: reference<T> | false = this.getReference<T>(this.getName(instance, options));
     if(reference == false) { throw new Error('Reference could not be found'); }
     
     try {
@@ -29,33 +84,46 @@ export class Container extends Singleton {
   }
 
   //Return a reference either as singleton, static class or object. If not existing it will be created
-  public get<T extends { name: string }>(instance: instanceType<T>, options: refererenceOptions): T {
-    let name = this.getName(instance, options);
+  public get<T>(instance: instanceType<T>, options: refererenceOptions): T {
     let reference: reference<T> | false = this.getReference(this.getName(instance, options));
-    if(reference === false && typeof instance == 'function') {
-      reference = {
-        name,
-        instance,
-        ...({ args: options.args })
-      }
+    if(reference === false && typeof instance !== 'string' && typeof instance !== 'object') {
+      return this.set<T>(instance, options);
     }
   }
 
   //Sets reference
-  public set<T>(instance: T, options: refererenceOptions) {
-    //this.references = 
+  public set<T>(instance: new() => T, options: refererenceOptions): reference<T> {
+    let name = this.getName(instance, options);
+    let referenceIndex: number = this.getReferenceIndexByName(name);
+    
+    let reference: reference<T> = {
+      instance: instance,
+      name,
+      ...({ args: options.args })
+    }
+    
+    if(referenceIndex != -1) {
+      this.references[referenceIndex] == reference;
+    } else {
+      this.references.push(reference)
+    }
+
+    return reference.instance;
   }
 
-  private getName<T extends { name: string }>(instance: instanceType<T>, options: refererenceOptions): string {
+  private getName<T>(instance: instanceType<T>, options: refererenceOptions): string {
     /*Determine name of passed instance. Either the name itself is passed as the instance
     parameter, the instance is a type with property "name" or name is passed in options*/
-    if(typeof instance == 'function' && !options.name) { instance.name }
+    /*if(typeof instance == 'function' && !options.name) { instance.name }
     else if(options.name) { return options.name; }
     else if(typeof instance == 'string') { return instance; }
     else { throw new Error('Instance name could not be determined'); }
   }
-  private getReference(name: string): reference | false {
-    let referenceIndex: number = this.references.findIndex((reference: reference) => reference.name == name);
+  private getReferenceIndexByName<T>(name: string): number {
+    return this.references.findIndex((reference: reference<T>) => reference.name == name);
+  }
+  private getReferenceByName<T>(name: string): reference<T> | false {
+    let referenceIndex: number = this.getReferenceIndexByName<T>(name);
     
     return referenceIndex != -1 ? this.references[referenceIndex] : false;
   }
@@ -67,7 +135,7 @@ class Foo {
 
 }
 
-container.create<Foo>('Foo', {});
+container.create<Foo>('Foo', {});*/
 /*
 export type container = {
   type: containerType,
@@ -129,11 +197,20 @@ export class Factory {
         this.container[name].object = new this.container[name].instance(...(passArguments || this.container[name].passArguments));
       }
       return this.container[name];
-  }
-
+    }
     throw new Error('Injectable could not be found');
   }
-  }
+  /*public static inject(name: string): void;
+  public static inject(name: string, passArguments: any[]): void;
+  public static inject(instance: any, type: containerType): void;
+  public static inject(instance: any, type: containerType, passArguments: any[]): void;
+  public static inject(instance: any, type: containerType, { name, passArguments }?: { name?: string, passArguments?: any[]}): void
+  public static inject(nameOrInstance: string | any, passArgumentsOrType?: any[] | containerType, passArguments?: any[] | { name?: string, passArguments?: any[]}) {
+    if(name typeof 'string') {
+      Factory.get(name);
+    }
+  }*/
+/*}
 
 export const injector = (type: containerType = containerType.SINGLETON, {
   name,
