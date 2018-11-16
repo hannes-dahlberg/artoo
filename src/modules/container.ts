789b7a8d11dac0e341c8b89cc951bdd1bf3d6d7b
Hannes Dahlberg <hannes@dwik.se>
2018-11-17 00:51:04 +0100

    Epic updates to container with test

--- src/modules/container.ts
+++ src/modules/container.ts
@@ -1,27 +1,23 @@
 import { Singleton } from './singleton';
+import { ConstructorParametersType } from './helpers';
 
-type ConstructorParametersType<T extends new (...args: any[]) => any> = T extends new (...args: infer P) => any ? P : never;
-//export type classType<T, C extends new(...args: any[]) => T> = new(...args: ConstructorParametersType<C>) => T;
-export type classType<T, C extends new (...args: any[]) => any> = new(...args: ConstructorParametersType<C>) => T
-class Apa {
-  constructor(a: number, b: string) {}
-}
+export type classType<T, C extends new (...args: any[]) => T> = new(...args: ConstructorParametersType<C>) => T
 
-type reference<T, C extends new (...args: any[]) => any> = {
+type reference<T, C extends new (...args: any[]) => T> = {
   name: string,
-  instance: classType<T, C> | any,
+  instance: C | T,
   object?: T
 }
 
 class Container extends Singleton {
   private references: reference<any,any>[] = [];
   
-  public create<T, C extends new (...args: any[]) => any>(name: string | classType<T, C>, args?: ConstructorParametersType<C>): T {
+  public create<T, C extends new (...args: any[]) => T>(name: string | C, args?: ConstructorParametersType<C>): T {
     let reference: reference<T, C>;
     if(typeof name !== 'string') {
       reference = this.getReference<T, C>(name.name);
       if(reference === undefined) {
-        this.set<T, C>(name.name, name);
+        this.setInternal(name.name, name);
       }
 
       return this.create<T, C>(name.name, args);
@@ -32,22 +28,33 @@ class Container extends Singleton {
     if(reference === undefined) {
       throw new Error('Reference could not be found');
     }
-    return new (<classType<T, C>>reference.instance)(...args);
+    
+    return new (<C>reference.instance)(...args);
   }
 
-  public get<T>(name: string): T
-  public get<T, C extends new (...args: any[]) => any>(name: string | classType<T, C>): T | C {
-    if(typeof name !== 'string') {
-      try {
-        name = name.name;
-      } catch(e) { throw new Error('Could not get name reference'); }
+  public get<T, C extends new (...args: any[]) => T>(name: string, instance: C): C;
+  public get<T>(name: T): T;
+  public get<T>(name: string, instance: T): T;
+  public get<T, C extends new (...args: any[]) => T>(name: string | C, instance?: C | T): C | T {
+    if(typeof name === 'function' && name.name !== undefined) {
+      let reference = this.getReference<T, C>(name.name);
+      if(reference === undefined) {
+        return this.setInternal(name.name, name);
+      } else {
+        return reference.instance;
+      }
+    } else if(typeof name === 'string' && instance !== undefined) {
+      return this.setInternal(name, instance);
+    } else if(typeof name !== 'string') {
+      throw new Error('Reference is missing name attribute to be created automatically. Please use get(name: string, instance: any)');
     }
+    
     return this.getReference<T, C>(name).instance;
   }
-  public getService<T, C extends new (...args: any[]) => any>(name: string | classType<T, C>, args?: ConstructorParametersType<C>): T {
+  public getService<T, C extends new (...args: any[]) => T>(name: string | C, args?: ConstructorParametersType<C>): T {
     let reference: reference<T, C> = this.getReference<T, C>(typeof name !== 'string' ? name.name : name);
     if(typeof name !== 'string' && reference === undefined) {
-        this.set<T, C>(name.name, name);
+        this.setInternal(name.name, name);
         return this.getService<T, C>(name.name, args);
     }
 
@@ -58,25 +65,30 @@ class Container extends Singleton {
 
     return reference.object;
   }
-
-  public set<T, C extends new (...args: any[]) => any>(name: string, instance: classType<T, C> | any): T {
-    let newReference: reference<T, C> = {
+  public set<C>(name: string, instance: C): C {
+    let reference = this.getReference(name);
+    if(reference === undefined) {
+      console.warn(`REFERENCE "${name}" HAS ALREADY BEEN SET, INSTANCE MAY NOT BE WHAT YOU'RE EXPECTED`);
+    }
+    return this.setInternal<C>(name, instance);
+  }
+  private setInternal<C>(name: string, instance: C): C {
+    let newReference: reference<any,any> = {
       name,
       instance
     };
 
-    let reference = this.getReference<T, C>(name);
+    let reference = this.getReference(name);
     if(reference === undefined) {
-      
       this.references.push(newReference);
-    } else {
       reference = newReference;
+    } else {
+      //reference = newReference;
     }
-
-    return newReference.instance;
+    return <C>reference.instance;
   }
 
-  private getReference<T, C extends new (...args: any[]) => any>(name: string): reference<T, C> {
+  private getReference<T, C extends new (...args: any[]) => T>(name: string): reference<T, C> {
     return <reference<T, C>>this.references.find((reference: reference<T, C>) => reference.name == name);
   }
 }
