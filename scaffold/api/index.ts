d20c40350043a92d49ac3fbcf34abe4f0c9eae71
Hannes Dahlberg <hannes@dwik.se>
2018-11-25 21:16:47 +0100

    Updated scaffold for API to use cors

--- scaffold/api/index.ts
+++ scaffold/api/index.ts
@@ -3,7 +3,8 @@ import { routes } from './routes';
 let app: server.app = {
   domain: config.get('SPA_HOST', 'api.test.test'),
   type: 'api',
-  routes: routes
+  routes: routes,
+  corsConfig: config.get('API_HOST', null);
 };
 
 export default app;
