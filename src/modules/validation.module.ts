420ea3cd0b8cb9ba2e38aa96abdd9db7ce3fb8fd
Hannes Dahlberg <hannes@dwik.se>
2018-12-14 08:02:08 +0100

    Removed debugger

--- src/modules/validation.module.ts
+++ src/modules/validation.module.ts
@@ -39,10 +39,8 @@ export const validate = (
       return !validate(value[valueName], validations[valueName]);
     }) === -1 ? true : false;
   } else if (typeof value === "string" && validations instanceof Array) {
-    console.log("APA");
     return validations.findIndex((v: validation) => !validate(value, v)) === -1 ? true : false;
   } else if (typeof value === "string" && typeof validations === "function") {
-    console.log("NAME", validations.name);
     return validations.name !== "required" ? validations(value) : true;
   }
 
