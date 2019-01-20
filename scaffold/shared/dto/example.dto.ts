0a64aa8c09c9aac9d17654b54bb8a155bdccc618
Hannes Dahlberg <hannes@dwik.se>
2019-01-20 11:20:26 +0100

    Updates scaffold DTO files with lint fixes

--- scaffold/shared/dto/example.dto.ts
+++ scaffold/shared/dto/example.dto.ts
@@ -10,6 +10,14 @@ export interface IExampleDTO extends IExample<IExampleDTO, IExampleDTO> { }
 export interface IExampleJSON extends IExample<IExampleJSON, IExampleJSON> { }
 
 export class ExampleDTO extends DTO<IExampleDTO> implements IExampleDTO {
+
+  public static parse(object: IExampleJSON): ExampleDTO {
+    return new ExampleDTO({
+      id: object.id,
+      ...(object.foo ? { foo: ExampleDTO.parse(object.foo) } : null),
+      ...(object.bar ? { projects: object.bar.map((bar: IExampleJSON) => ExampleDTO.parse(bar)) } : null),
+    });
+  }
   public id: number;
   public foo?: ExampleDTO;
   public bar?: ExampleDTO[];
@@ -19,14 +27,6 @@ export class ExampleDTO extends DTO<IExampleDTO> implements IExampleDTO {
       id: this.id,
       ...(this.foo ? { foo: this.foo.serialize() } : null),
       ...(this.bar ? { bar: this.bar.map((foo: ExampleDTO) => foo.serialize()) } : null),
-    }
-  }
-
-  public static parse(object: IExampleJSON): ExampleDTO {
-    return new ExampleDTO({
-      id: object.id,
-      ...(object.foo ? { foo: ExampleDTO.parse(object.foo) } : null),
-      ...(object.bar ? { projects: object.bar.map((bar: IExampleJSON) => ExampleDTO.parse(bar)) } : null)
-    });
+    };
   }
 }
